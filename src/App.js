00f66fafaf0ef73c99167b9f48596d85172350f8
Annisa Sazali <annisa.sazali@gmail.com>
2025-08-21 12:57:19 +0800

    Testing change from Feature branch.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing GH workflow.
+          Testing code change from Feature branch.
         </p>
         <a
           className="App-link"
